242c77510541295664efc5e19063f97c21d2500a
SplashyCoder <pachecomorahectordavid@gmail.com>
2024-10-20 16:45:50 -0500

    error codes added

--- users.py
+++ users.py
@@ -1,9 +1,8 @@
-from fastapi import  FastAPI
+from fastapi import  FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
-
-# print(app)
+# Inicia el server: uvicorn users:app --reload
 
 class User(BaseModel):
     id: int
@@ -14,7 +13,11 @@ class User(BaseModel):
 users_list = [
     User(id=1, username='David', surname='Pacheco', url='https://google.com'),
     User(id=2,username='Alejandro', surname='Mora', url='https://facebook.com'),
-    User(id=3,username='Juan', surname='Sanchez', url='https://twitter.com'),
+    User(id=4,username='Splashy', surname='Gnu', url='https://twitter.com'),
+    User(id=5,username='Oreo', surname='Gordita', url='https://twitter.com'),
+    User(id=6,username='Moncho', surname='Gordito', url='https://twitter.com'),
+    User(id=7,username='El tren', surname='Fofo', url='https://twitter.com'),
+
 ]
 
 @app.get('/usersjson')
@@ -40,17 +43,48 @@ async def users() :
 @app.get('/user/{id}')
 async def user(id:int):
     return search_user(id)
-    # users = filter(lambda user: user.id == id, users_list)
-    
-    # try:
-    #     return list(users)[0]
-    # except:
-    #     return {'error':'User not found'}
 
 @app.get('/user/')
 async def user(id:int):
     return search_user(id)
+    
+@app.post('/user/',response_model=User,status_code=201)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code=404, detail="El usuario ya existe")
+        
+        
+    users_list.append(user)
+    return user
+        
 
+@app.delete('/user/')
+async def user(id:int):
+    found = False
+    for index,saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+            return user
+
+    if found == False:
+        return {'error':'User not deleted'}
+        
+
+@app.put('/user/')
+async def user(user:User):
+    found = False
+    
+    for index,saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+            return user
+    
+    if not found:
+        return {'error':'User not updated'}
+
+    
 def search_user(id:int):
     users = filter(lambda user: user.id == id, users_list)
     
