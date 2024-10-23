610fc885243b50f3e4334656dae1854a1f38fbd7
SplashyCoder <pachecomorahectordavid.com>
2024-10-23 18:59:57 -0500

    Static resources added

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import  FastAPI
 from routers import products, users, user
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(user.router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 
